42c838f5892fca104d395c23239af6c7d499b940
Roman Zamolotov <rzamolotov@gmail.com>
2022-07-26 19:27:48 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Роман Замолотов on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
